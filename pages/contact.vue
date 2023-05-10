d1a2f3ee2d4751c9c1790f7f797aea6177a6da73
Colin Peyrat <hello@colinpeyrat.com>
2023-05-10 11:52:44 +0200

    initial commit

---
+++ pages/contact.vue
@@ -0,0 +1,6 @@
+<template>
+  <main>
+    <h1>Contact</h1>
+    <a href="mailto:colinpeyrat@gmail.com">colinpeyrat@gmail.com</a>
+  </main>
+</template>
---
+++ pages/products/[id].vue
@@ -0,0 +1,12 @@
+<template>
+  <main>
+    <h1>Produit #{{ id }}</h1>
+    <nuxt-link to="/">Home</nuxt-link>
+  </main>
+</template>
+
+<script setup lang="ts">
+const route = useRoute()
+
+const { id } = route.params
+</script>
